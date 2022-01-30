907f6d2db828776d5631f717e5bc7b1be2228e0d
BEAST <beast@beast.com>
2022-01-30 13:55:01 -0600

    - ‘plugin.video.genesisreborn/LICENSE.txt’ - ‘plugin.video.genesisreborn/README.md’ - ‘plugin.video.genesisreborn/addon.xml’ - ‘plugin.video.genesisreborn/fanart.jpg’ - ‘plugin.video.genesisreborn/genesisreborn.py’ - ‘plugin.video.genesisreborn/icon.png’ - ‘plugin.video.genesisreborn/plugin.video.genesisreborn-2018.04.21.zip’ - ‘plugin.video.genesisreborn/resources/__init__.py’ - ‘plugin.video.genesisreborn/resources/__init__.pyo’ - ‘plugin.video.genesisreborn/resources/language/English (US)/strings.po’ ...

--- plugin.video.pelisalacarta/lib/sambatools/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#------------------------------------------------------------
-# pelisalacarta
-# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-#------------------------------------------------------------
-import os
-import sys
-sys.path.append(os.path.dirname(__file__))
--- plugin.video.pelisalacarta/servers/debriders/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#------------------------------------------------------------
-# pelisalacarta - XBMC Plugin
-# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-#------------------------------------------------------------
